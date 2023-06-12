5c6f992d24251a5842e9c06fbb5bc6bea0a0d2dc
NastiaAnn <anastasianazar4@gmail.com>
2023-06-12 17:09:34 +0300

    нфі

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,20 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <nav>
+        <NavLink to="/">Home page</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Тут буде домашня сторінка</div>} />
+        <Route
+          path="/movies"
+          element={<div>тут буде сторінка пошуку фільмів</div>}
+        />
+      </Routes>
     </div>
   );
 };
