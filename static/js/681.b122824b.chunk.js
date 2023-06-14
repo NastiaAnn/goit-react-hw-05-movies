561da439c203b3399e0fefd6d983b1f3343c4a10
NastiaAnn <115037225+NastiaAnn@users.noreply.github.com>
2023-06-14 21:16:54 +0000

    Deploying to gh-pages from @ NastiaAnn/goit-react-hw-05-movies@4d6f8d5bd49a8f7980a0d0805e931a3a78707ed8 🚀

--- static/js/681.b122824b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[681],{681:function(e,t,o){o.r(t);var r=o(689),c=o(184);t.default=function(){var e=(0,r.UO)().movieId;return console.log(e),(0,c.jsx)("section",{children:(0,c.jsx)("div",{children:"Review of the film"})})}}}]);
-//# sourceMappingURL=681.b122824b.chunk.js.map
