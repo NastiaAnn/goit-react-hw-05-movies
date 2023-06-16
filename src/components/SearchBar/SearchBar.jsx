0a5799e4bf314d3976a8c90c9d710f3aa33babc1
NastiaAnn <anastasianazar4@gmail.com>
2023-06-16 12:18:50 +0300

    before input update

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,61 @@
 import { FormLabel, FormText, SubmitButton, SearchForm } from './styled';
+import { useSearchParams, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { handleMoviesSearch } from 'services/TmdbApi';
+import { MoviesList } from 'components/MoviesList/MoviesList';
 
-export const SearchBar = ({ handleSubmit, updateString }) => {
+export const SearchBar = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') ?? '';
+  const location = useLocation();
+
+  useEffect(() => {
+    if (query === null) return;
+
+    getSearchedMovies(query);
+  }, [query]);
+
+  const getSearchedMovies = async query => {
+    try {
+      const searchedMovies = await handleMoviesSearch(query);
+      setMovies(searchedMovies.data.results);
+    } catch (error) {
+      console.log('Error');
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    setSearchParams({ query: form.elements.query.value });
+  };
+
+  const updateQueryString = event => {
+    const movieIdValue = event.target.value;
+    if (movieIdValue === '') {
+      return setSearchParams({});
+    }
+    if (query.trim() === '') {
+      alert('Empty line');
+      setSearchParams({});
+      return;
+    }
+    setSearchParams({ query: movieIdValue });
+  };
+
   return (
-    <SearchForm onSubmit={handleSubmit}>
-      <FormLabel>
-        <FormText type="text" name="query" onChange={updateString} />
-      </FormLabel>
-      <SubmitButton type="submit">Search</SubmitButton>
-    </SearchForm>
+    <div>
+      {' '}
+      <SearchForm onSubmit={handleSubmit}>
+        <FormLabel>
+          <FormText type="text" name="query" onChange={updateQueryString} />
+        </FormLabel>
+        <SubmitButton type="submit">Search</SubmitButton>
+      </SearchForm>
+      <MoviesList movies={movies} location={location} />
+    </div>
   );
 };
+
+export default SearchBar;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,56 +1,7 @@
-import { useSearchParams, useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { handleMoviesSearch } from 'services/TmdbApi';
 import { SearchBar } from 'components/SearchBar/SearchBar';
-import { MoviesList } from 'components/MoviesList/MoviesList';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const Movies = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const query = searchParams.get('query') ?? '';
-  const location = useLocation();
-
-  useEffect(() => {
-    if (query === null)
-      // return Notify.warning(
-      //   'Empty line. Please enter something in the search field'
-      // );
-
-      getSearchedMovies(query);
-  }, [query]);
-
-  const getSearchedMovies = async query => {
-    try {
-      const searchedMovies = await handleMoviesSearch(query);
-      setMovies(searchedMovies.data.results);
-    } catch (error) {
-      console.log('Error');
-    }
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    setSearchParams({ query: form.elements.query.value });
-    form.reset();
-  };
-
-  const updateQueryString = event => {
-    const movieIdValue = event.target.value;
-    if (movieIdValue === '') {
-      return setSearchParams({});
-    }
-    setSearchParams({ query: movieIdValue });
-  };
-
-  return (
-    <div>
-      <SearchBar handleSubmit={handleSubmit} updateString={updateQueryString} />
-      <MoviesList movies={movies} location={location} />
-    </div>
-  );
+  return <SearchBar />;
 };
 
 export default Movies;
